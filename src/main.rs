8555c1a39f3bb3261dc834eb31fef95a29719f2a
Michael Sievers <michael@hamburg.de>
2026-05-30 13:23:39 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Write, stdin};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    stdin()
+        .read_line(&mut command)
+        .expect("Could not read input");
+
+    println!("{{{}}}: command not found", command.trim());
+
 }
