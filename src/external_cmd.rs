c13fa7c0b5740a5983e023d391b853962dadae87
Michael Sievers <michael@hamburg.de>
2026-06-07 11:33:26 +0200

    Execute external commands

--- src/external_cmd.rs
+++ src/external_cmd.rs
@@ -1,4 +1,6 @@
-use std::io::{self, Error};
+use crate::util::find_executable_in_path;
+use std::io::{self, Error, Write};
+use std::process::Command;
 
 #[derive(Debug, PartialEq)]
 pub struct ExternalCmd {
@@ -8,7 +10,22 @@ pub struct ExternalCmd {
 
 impl ExternalCmd {
     pub fn run(&self) -> io::Result<i32> {
-        println!("{}: command not found", self.name);
-        Err(Error::from_raw_os_error(1))
+        match find_executable_in_path(&self.name) {
+            Some(full_path) => match Command::new(&full_path).args(&self.parameters).output() {
+                Ok(output) => {
+                    io::stdout().write_all(&output.stdout)?;
+                    io::stderr().write_all(&output.stderr)?;
+                    return Err(Error::from_raw_os_error(output.status.code().unwrap_or(1)));
+                }
+                Err(_) => {
+                    println!("Failed to run command {}", full_path);
+                    return Err(Error::from_raw_os_error(1));
+                }
+            },
+            None => {
+                println!("{}: command not found", self.name);
+                return Err(Error::from_raw_os_error(1));
+            }
+        }
     }
 }
